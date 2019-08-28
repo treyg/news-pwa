6f59888c8b34a63e19a10b060d49d2386007a58e
treyg21 <41927921+treyg21@users.noreply.github.com>
2019-08-27 20:40:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+
+
+const apiKey = `16db718aff1b4ba0beb33c3583af5f7a`
+const main = document.querySelector('main');
+const sourceSelector = document.querySelector('#sourceSelector');
+const defaultSource = `the-washington-post`;
+
+window.addEventListener('load', async e =>{
+    updateNews();
+    await updateSources();
+    sourceSelector.value = defaultSource;
+
+    sourceSelector.addEventListener('change', e =>{
+        updateNews(e.target.value);
+    }) 
+
+    if(`serviceWorker` in navigator) {
+        try {
+            navigator.serviceWorker.register('sw.js')
+            console.log(`sw registered`)
+        } catch (error) {
+            console.log(`sw reg fail`)
+            
+        }
+    }
+});
+
+async function updateSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?language=en&apiKey=16db718aff1b4ba0beb33c3583af5f7a`);
+    const json = await res.json();
+
+    sourceSelector.innerHTML = json.sources.
+    map(src => `<option value="${src.id}">${src.name}</option>`).join(`\n`);
+}
+
+
+async function updateNews(source = defaultSource) {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`)
+    const json = await res.json();
+
+    main.innerHTML = json.articles.map(createArticle).join('\n');
+}
+
+function createArticle(article){
+    return `
+    <div class="article">
+      <a href="${article.url}">
+        <h2>${article.title}</h2>
+        <img src="${article.urlToImage}" alt="${article.title}">
+        <p>${article.description}</p>
+      </a>
+    </div>
+  `;
+}
